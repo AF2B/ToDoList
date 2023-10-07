3d81c0233560f11cd9607a395595f4d95242c4cb
andre.borba <andre.borbaaf2b@gmail.com>
2023-10-07 19:30:47 -0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by André Filipe Fonsêca Borba on 07/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
